51f6103e4b135ffc42fb022c2dc00a2ac77c23fb
kkm4512 <nayoun440@naver.com>
2024-08-11 23:31:09 +0900

    init

---
+++ src/test/java/com/calendarapp/calendarapp/CalendarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendarapp.calendarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
