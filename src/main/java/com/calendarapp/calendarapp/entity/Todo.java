bb34e7688663d908b279d3045ee2965134c1471a
kkm4512 <nayoun440@naver.com>
2024-08-13 16:34:42 +0900

    remove. 삭제

---
+++ src/main/java/com/calendarapp/calendarapp/entity/Todo.java
@@ -0,0 +1,13 @@
+package com.calendarapp.calendarapp.entity;
+
+import java.time.LocalDate;
+
+
+public class Todo {
+    private Long todoId;
+    private Long memberId;
+    private String todo;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+}
