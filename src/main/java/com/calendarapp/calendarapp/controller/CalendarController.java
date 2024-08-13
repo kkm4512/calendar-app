12785d18ae2f63f1fa401e46cc6b4260ad1cdd64
kkm4512 <nayoun440@naver.com>
2024-08-13 11:33:37 +0900

    fix. Rest ful 한 Api로 변경 1. 그에 따른 Read.me 파일 수정

--- src/main/java/com/calendarapp/calendarapp/controller/CalendarController.java
+++ src/main/java/com/calendarapp/calendarapp/controller/CalendarController.java
@@ -20,42 +20,45 @@ public class CalendarController {
         this.calendarService = calendarService;
     }
 
-    @GetMapping("/findAll")
+    //모든 일정 조회
+    @GetMapping
     public List<CalendarResponseDto> getAllCalendars(){
         return calendarService.getAllCalendars();
     }
 
-    @GetMapping("/findById/{id}")
+    // 아이디로 단일 일정 조회
+    @GetMapping("/{id}")
     public CalendarResponseDto getCalendarById(@PathVariable Long id){
         return calendarService.getCalendarById(id);
     }
 
     // 수정일로 전체 일정 조회
-    @GetMapping("/findByUpdateAt/{updateAt}")
-    public List<CalendarResponseDto> getCalendarByUpdateAt(@PathVariable String updateAt){
+    @GetMapping("/updateAt")
+    public List<CalendarResponseDto> getCalendarByUpdateAt(@RequestParam String updateAt){
         return calendarService.getCalendarByUpdateAt(updateAt);
     }
 
     // 담당자명으로 전체 일정 조회
-    @GetMapping("/findByAuthor/{author}")
-    public List<CalendarResponseDto> getCalendarByAuthor(@PathVariable String author){
+    @GetMapping("/author")
+    public List<CalendarResponseDto> getCalendarByAuthor(@RequestParam String author){
         return calendarService.getCalendarByAuthor(author);
     }
 
-    @PostMapping("/save")
+    //일정 저장
+    @PostMapping
     public CalendarResponseDto saveCalendar(@RequestBody CalendarRequestDto calendarRequestDto){
         return calendarService.saveCalendar(calendarRequestDto);
     }
 
     //할일내용, 담당자명 수정
     //수정 진행시, 내부적으로 updateAt 자동 업데이트
-    @PutMapping("/update/{id}")
+    @PutMapping("/{id}")
     public CalendarResponseDto updateCalendar(@PathVariable Long id, @RequestBody CalendarRequestUpdateDto calendarRequestUpdateDto){
         return calendarService.updateCalendar(id,calendarRequestUpdateDto);
     }
 
     //할일 삭제
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public boolean deleteCalendar(@PathVariable Long id){
         return calendarService.deleteCalendar(id);
     }
